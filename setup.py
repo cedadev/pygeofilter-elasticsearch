fdfd8aa9b3c1df7ea4c3645e4d49194246e98156
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-07-12 14:57:20 +0100

    removing python version requirement

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Security',
@@ -63,9 +62,6 @@ setup(
 
     license=__license__,
 
-    # This qualifier can be used to selectively exclude Python versions -
-    # in this case early Python 2 and 3 releases
-    python_requires='>=3.6',
     install_requires=requirements,
     long_description=_long_description,
     long_description_content_type='text/markdown',
